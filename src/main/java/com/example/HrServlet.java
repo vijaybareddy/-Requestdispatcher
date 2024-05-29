06d64bff9141a0b9da57b743027f74aa671b703c
vijaybareddy <vijaybareddy143@gmail.com>
2024-05-29 17:39:26 +0530

    Vijay

--- src/main/java/com/example/HrServlet.java
+++ src/main/java/com/example/HrServlet.java
@@ -2,15 +2,18 @@ package com.example;
 
 
     import java.io.IOException;
+    import javax.servlet.RequestDispatcher;
     import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-    @WebServlet("/FrontServlet/HrServlet")
+   // @WebServlet("/FrontServlet/HrServlet")
     public class HrServlet extends HttpServlet {
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
             response.getWriter().write("This is the HR Servlet");
+            RequestDispatcher r= request.getRequestDispatcher("/ProjectServlet");
+            r.forward(request,response);
         }
 }
--- src/main/java/com/example/ProjectServlet.java
+++ src/main/java/com/example/ProjectServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-    @WebServlet("/FrontServlet/HrServlet/ProjectServlet")
+   // @WebServlet("/FrontServlet/HrServlet/ProjectServlet")
     public class ProjectServlet extends HttpServlet {
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
             response.getWriter().write("This is the Project Servlet");
