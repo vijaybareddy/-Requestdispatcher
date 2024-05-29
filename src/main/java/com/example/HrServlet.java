79009181fb407a7e88382f659cff21234f5d7f3e
vijaybareddy <vijaybareddy143@gmail.com>
2024-05-29 16:34:00 +0530

    Vijay

---
+++ src/main/java/com/example/HrServlet.java
@@ -0,0 +1,16 @@
+package com.example;
+
+
+    import java.io.IOException;
+    import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+    @WebServlet("/FrontServlet/HrServlet")
+    public class HrServlet extends HttpServlet {
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.getWriter().write("This is the HR Servlet");
+        }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Servlet Navigation</title>
+</head>
+<body>
+    <h1>Servlet Navigation</h1>
+    <ul>
+        <li><a href="FrontServlet">FrontServlet</a></li>
+        <li><a href="FrontServlet/HrServlet">HrServlet</a></li>
+        <li><a href="FrontServlet/HrServlet/ProjectServlet">FrontServlet -> HrServlet -> ProjectServlet</a></li>
+    </ul>
+</body>
+</html>
